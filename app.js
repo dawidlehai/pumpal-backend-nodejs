5d274756181794af2dc70b6a11e7e73fb567beab
dawidlehai <haidawidle@gmail.com>
2023-04-30 13:13:11 +0200

    feat: move server logic to server.js

--- app.js
+++ app.js
@@ -2,14 +2,11 @@ const express = require("express");
 const morgan = require("morgan");
 
 const app = express();
-const port = 3000;
-
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
 
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.status(200).send("It's working!");
 });
+
+module.exports = app;
