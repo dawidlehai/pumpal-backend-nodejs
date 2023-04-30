07ceae67c5abd75c1f744eba91f7584f4e4d8442
dawidlehai <haidawidle@gmail.com>
2023-04-30 20:43:29 +0200

    feat: add error handling for duplicate field value

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,16 @@
+const AppError = require("./../utils/appError");
+
+module.exports = (err, req, res, next) => {
+  err.statusCode ??= 500;
+  err.status ??= "error";
+
+  if (process.env.NODE_ENV === "development") sendErrDev(err, req, res);
+  if (process.env.NODE_ENV === "production") {
+    if (err.code === 11000) err = handleDuplicateFieldValue(err);
+    sendErrProd(err, req, res);
+  }
+};
+
 function sendErrDev(err, req, res) {
   res.status(err.statusCode).json({
     status: err.status,
@@ -12,7 +25,8 @@ function sendErrProd(err, req, res) {
   if (err.isOperational === true)
     res.status(err.statusCode).json({
       status: err.status,
-      message: err.message,
+      message: err.status === "error" ? err.message : undefined,
+      data: err.data,
     });
   else {
     console.log("Nonoperational internal server error:", err);
@@ -24,10 +38,12 @@ function sendErrProd(err, req, res) {
   }
 }
 
-module.exports = (err, req, res, next) => {
-  err.statusCode ??= 500;
-  err.status ??= "error";
+function handleDuplicateFieldValue(err) {
+  const [[key, value]] = Object.entries(err.keyValue);
+  let message = `The value '${value}' for the '${key}' field already exists. It should be unique. Please provide another value.`;
 
-  if (process.env.NODE_ENV === "development") sendErrDev(err, req, res);
-  if (process.env.NODE_ENV === "production") sendErrProd(err, req, res);
-};
+  if (key === "username")
+    message = `The username '${value}' is already taken. Please choose a different username that is not already in use by another user.`;
+
+  return new AppError(message, 400, "fail", { [key]: message });
+}
--- utils/appError.js
+++ utils/appError.js
@@ -1,10 +1,11 @@
 class AppError extends Error {
-  constructor(message, statusCode, status) {
+  constructor(message, statusCode, status = "fail", data) {
     super(message);
 
     this.statusCode = statusCode;
     this.status = status;
     this.isOperational = true;
+    this.data = data;
 
     Error.captureStackTrace(this, this.constructor);
   }
