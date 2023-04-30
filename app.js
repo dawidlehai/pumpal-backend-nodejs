a6a4a8934fd75921c028673c0722ae9cd8ac4efd
dawidlehai <haidawidle@gmail.com>
2023-04-30 19:11:31 +0200

    feat: add better error handling with globalErrorHandler

--- app.js
+++ app.js
@@ -4,16 +4,22 @@ const morgan = require("morgan");
 const app = express();
 
 const userRouter = require("./routes/userRoutes");
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
 
 app.use(morgan("dev"));
-
-app.get("/", (req, res) => {
-  res.status(200).send("It's working!");
-});
 
 app.use(express.json());
 
 const apiBase = "/api";
 app.use(`${apiBase}/users`, userRouter);
+
+app.all("*", (req, res, next) => {
+  next(
+    new AppError(`Cannot find ${req.originalUrl} on this server.`, 404, "error")
+  );
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
